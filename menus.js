59c50c51774b8b7746295234cb4f860645579338
anikoloudis <a.a.nikoloudis@student.tudelft.nl>
2025-05-14 00:24:29 +0200

    most updated

---
+++ menus.js
@@ -0,0 +1,10 @@
+// menus.js
+document.getElementById('breakfast-btn').addEventListener('click', () => {
+    // navigate to the /breakfast page
+    window.location.href = '/breakfast.html';
+  });
+  
+  document.getElementById('dinner-btn').addEventListener('click', () => {
+    // navigate to the /dine page
+    window.location.href = '/dine.html';
+  });
